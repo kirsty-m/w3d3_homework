a154da7bcc18f0cec7926d776cf28ddee7dc7e76
Kirsty McBride <kirsty@Kirstys-MacBook-Pro.local>
2023-05-31 15:17:41 +0100

    second commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,10 +2,13 @@ from flask import render_template
 from app import app
 from models.order_list import orders
 
+
+
 @app.route('/')
-def show_orders():
+def show_orders_name():
     return render_template("index.html", order_list = orders)
 
-@app.route('/orders/<1>')
-def show_one_order():
-    return render_template("index.html", )
+@app.route('/order/<index>')
+def show_order_details(index):
+    return render_template("order.html", single_order = orders[int(index)])
+
